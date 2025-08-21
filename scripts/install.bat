d3ddd832612a7871751fac42841b50b3e8adecab
McSnurtle <joepro17@outlook.com>
2025-08-21 10:37:13 -0400

    Create scripts/install.bat
    - Ported install.sh to windows systems

---
+++ scripts/install.bat
@@ -0,0 +1,17 @@
+@echo OFF
+
+echo "Running installation for https://github.com/McSnurtle/gbro.git"
+
+set SCRIPT_DIR=%~dp0
+cd /d "%SCRIPT_DIR%\.."
+
+# python installations
+@for /f "tokens=*" %%i in ('python3 --version 2^>^&1') do @echo Using %%i
+python3 -m venv venv
+call .\venv\Script\activate
+echo "Upgrading dependencies..."
+pip install --upgrade --verbose -r requirements.txt
+deactivate
+
+pause
+
