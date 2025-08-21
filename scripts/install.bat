7516fd0d6bc7a7466afe986acca943d9d504df1b
McSnurtle <93739492+McSnurtle@users.noreply.github.com>
2025-08-21 12:14:48 -0400

    Fix install.bat formatting error
    - Changed commenting style to be consistent with batch script.

--- scripts/install.bat
+++ scripts/install.bat
@@ -5,7 +5,7 @@ echo "Running installation for https://github.com/McSnurtle/gbro.git"
 set SCRIPT_DIR=%~dp0
 cd /d "%SCRIPT_DIR%\.."
 
-# python installations
+:: python installations
 @for /f "tokens=*" %%i in ('python3 --version 2^>^&1') do @echo Using %%i
 python3 -m venv venv
 call .\venv\Script\activate
