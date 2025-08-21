b6128f17f7b47ff598374dd1f2dcf7f424d87abe
McSnurtle <joepro17@outlook.com>
2025-08-21 12:18:19 -0400

    Fix scripts/install.bat typo

--- scripts/install.bat
+++ scripts/install.bat
@@ -8,7 +8,7 @@ cd /d "%SCRIPT_DIR%\.."
 :: python installations
 @for /f "tokens=*" %%i in ('python3 --version 2^>^&1') do @echo Using %%i
 python3 -m venv venv
-call .\venv\Script\activate
+call .\venv\Scripts\activate
 echo "Upgrading dependencies..."
 pip install --upgrade --verbose -r requirements.txt
 deactivate
